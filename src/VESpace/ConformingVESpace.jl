a39e2d593695f596d0553f6bcbbaaa549c2c4996
Balaje K <balaje6@gmail.com>
2021-09-28 15:26:12 +1000

    Remove L2 projector and integration

--- src/VESpace/ConformingVESpace.jl
+++ src/VESpace/ConformingVESpace.jl
@@ -80,61 +80,10 @@ function _generate_ritz_matrices(verts, acd)
   B, D
 end
 
-function _generate_l2_matrices(verts, acd)
-  npolys = 3
-  H = zeros(npolys, npolys)
-  vs = map(nc -> SVector(Tuple(nc)), verts)
-  vs = reinterpret(reshape, Float64, vs)
-  modwrap = (x,a) -> mod(x-1, a) + 1
-  a,c,d=acd
-  nsides=length(verts)
-  linear_polynomials=[[0,0],[1,0],[0,1]]
-  H[1,1] = a
-  for j=2:npolys
-    for k=2:npolys
-      for v=1:nsides
-        vert = vs[:,v]
-        next = vs[:,modwrap(v-1,nsides)]
-        H[j,k] = H[j,k] + _integrate_scaled_monomials_on_triangle([c, vert, next], linear_polynomials[j], linear_polynomials[k], d)
-      end
-    end
-  end
-  H
-end
-
 
 function _generate_stabilising_term(Π∇, acd)
   B, D = Π∇
   projector = (B*D)\B
   nsides = size(B,2)
   (I(nsides) - D*projector)'*(I(nsides) - D*projector)
-end
-
-"""
-For H matrix: Split polygons and integrate
-"""
-function _integrate_scaled_monomials_on_triangle(C, α, β, diameter)
-  P1=C[1]
-  P2=C[2]
-  P3=C[3]
-
-  centroid = P1
-  M = [(1., Tuple(P1)...), (1., Tuple(P2)...), (1., Tuple(P3)...)]
-  M = reinterpret(reshape, Float64, M)'
-  area = 0.5*abs(det(M))
-
-  Q = quadrature_rule(2)
-  qw = Q[1]
-  qx = Q[2]
-  qy = Q[3]
-
-  V = 0
-  for q=1:length(qx)
-    xhat = (P2[1]-P1[1])*qx[q] + (P3[1]-P1[1])*qy[q] + P1[1];
-    yhat = (P2[2]-P1[2])*qx[q] + (P3[2]-P1[2])*qy[q] + P1[2];
-    ma = ((xhat-centroid[1])/diameter)^(α[1])*((yhat-centroid[2])/diameter)^(α[2]);
-    mb = ((xhat-centroid[1])/diameter)^(β[1])*((yhat-centroid[2])/diameter)^(β[2]);
-    V = V + qw[q]*2*area*ma*mb
-  end
-  V
 end
